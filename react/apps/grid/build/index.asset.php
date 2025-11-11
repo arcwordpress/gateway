38171ddbe5cebe82ebac9bc1723bb3c8434dafb8
Casey Milne <cjmofficio@gmail.com>
2025-11-11 09:09:30 -0800

    Remove unused field package.

--- react/apps/grid/build/index.asset.php
+++ react/apps/grid/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '98d34db8c7e6802ddecc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'ab7dbdc167eac468be4f');
