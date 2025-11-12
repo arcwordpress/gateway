9a9a719cc3812f1720d3046a668014f96a24e97c
Casey Milne <cjmofficio@gmail.com>
2025-11-11 23:40:43 -0800

    Studio built with broken forms.

--- react/apps/studio/build/index.asset.php
+++ react/apps/studio/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '5d0669e61d35444528af');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '7558e801797a67008320');
