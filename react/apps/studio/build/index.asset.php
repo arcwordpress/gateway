d71045a392a03b5a250b8d17dc974270b579fd31
Casey Milne <cjmofficio@gmail.com>
2025-11-20 00:34:31 -0800

    Rebuild of studio with grid package updates.

--- react/apps/studio/build/index.asset.php
+++ react/apps/studio/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '6d9c8ac41acb39fdf543');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '8bdf8727bcfd22c96c7b');
