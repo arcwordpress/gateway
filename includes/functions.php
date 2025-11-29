f82768d0b7973edbefe56e081cbb1cd2692d2183
Casey Milne <cjmofficio@gmail.com>
2025-11-29 07:21:31 -0800

    Draft extension registry classes.

--- includes/functions.php
+++ includes/functions.php
@@ -3,3 +3,22 @@
 function gateway_core_active() {
     return class_exists('\Gateway\Plugin');
 }
+
+function gateway_extension_registry() {
+    return \Gateway\Extensions\ExtensionRegistry::instance();
+}
+
+function gateway_registered_extensions_array() {
+    $registry = gateway_extension_registry();
+    $extensions = $registry->getAll();
+
+    // Convert to array format (e.g., key, class name)
+    $result = [];
+    foreach ($extensions as $key => $extension) {
+        $result[] = [
+            'key' => $key,
+            'class' => get_class($extension),
+        ];
+    }
+    return $result;
+}
