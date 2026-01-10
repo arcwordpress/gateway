b37cca53d68159c4a7fb9032d14f039c26f9e6de
Casey Milne <casey@arcwp.ca>
2026-01-10 03:24:14 -0800

    Block build update.

---
+++ react/blocks/dataview/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-blocks', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '952c0126af61fe8703ed');
