18f7ed904b87f908587c77ffbbe93298ad650587
Casey Milne <casey@arcwp.ca>
2026-01-12 09:46:05 -0800

    Rebuild blocks.

--- react/blocks/field-list/build/index.asset.php
+++ react/blocks/field-list/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '68670768b58c7c26df92');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ba832bcc34360cb404e1');
--- react/blocks/gt1/build/index.asset.php
+++ react/blocks/gt1/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-server-side-render'), 'version' => 'e4e126c018a12dafcd72');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-server-side-render'), 'version' => '8ccea8e42e87eb830753');
