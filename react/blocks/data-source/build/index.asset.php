ae652db41715776df9c247cb8711454a056fc630
Casey Milne <casey@arcwp.ca>
2026-01-12 10:32:18 -0800

    Data source build.

--- react/blocks/data-source/build/index.asset.php
+++ react/blocks/data-source/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ee8fc6115b4ebec2239a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'eb98b327d538bb3aea1a');
---
+++ react/blocks/data-source/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-interactivity'), 'version' => 'f6ed3b2dddfb0a9b0822');
