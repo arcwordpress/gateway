654e199aa6120b6e377d15e6a2a09c7855a4df44
Casey Milne <casey@arcwp.ca>
2026-01-22 00:17:22 -0800

    Fix logo path.

--- react/apps/exta/build/index.asset.php
+++ react/apps/exta/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'f93f85fad815526ed6dc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '52175b1d71c8c9805d43');
