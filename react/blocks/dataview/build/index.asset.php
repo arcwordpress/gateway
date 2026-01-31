307f756c07cb89079249a817a7b0fed690be45f2
Casey Milne <casey@arcwp.ca>
2026-01-31 00:00:04 -0800

    Removed aspirational block types.

--- react/blocks/dataview/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-blocks', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '8a7a62cc6c96b0ec5099');
