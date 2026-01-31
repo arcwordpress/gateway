f3122d7440ab3a3969874d4aec690fe279805b55
Casey Milne <casey@arcwp.ca>
2026-01-31 04:33:31 -0800

    Rebuild bound string block.

--- js/blocks/bound-string/build/index.asset.php
+++ js/blocks/bound-string/build/index.asset.php
@@ -1,16 +1,1 @@
-<?php
-/**
- * Asset dependencies for bound-string block
- */
-return array(
-	'dependencies' => array(
-		'wp-block-editor',
-		'wp-blocks',
-		'wp-components',
-		'wp-data',
-		'wp-element',
-		'wp-i18n',
-		'wp-icons',
-	),
-	'version' => '0.1.0',
-);
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '01836f23be88c0fdee60');
