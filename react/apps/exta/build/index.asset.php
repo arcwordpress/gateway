e19a1075a03f9fd3ef9caf64da3209c15830c8f0
Casey Milne <casey@arcwp.ca>
2026-02-01 03:35:22 -0800

    Rebuild exta with create link restored.

--- react/apps/exta/build/index.asset.php
+++ react/apps/exta/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'e1c2f2856072f636ea30');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '3872ee67ba75511f545c');
