1fb70eb3ebcb27912b47cf3627becbe4e5ffe930
Casey Milne <casey@arcwp.ca>
2026-05-06 15:30:36 -0700

    Update build project.

--- react/apps/grid/build/index.asset.php
+++ react/apps/grid/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'f397137eebd2628441bf');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'c0a0edc62e916e1497e5');
--- react/apps/studio/build/index.asset.php
+++ react/apps/studio/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'e8c1e9b3f82c9ea4a057');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'e9c103332da00dd65d61');
