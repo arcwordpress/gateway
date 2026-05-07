2b18e1bea1a755e451766d3fd8ba246183b7d006
Casey Milne <casey@arcwp.ca>
2026-05-07 12:35:01 -0700

    Project build.

--- react/apps/studio/build/index.asset.php
+++ react/apps/studio/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '9b5f05208395cb519062');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'e180b428e2f2baa5199e');
