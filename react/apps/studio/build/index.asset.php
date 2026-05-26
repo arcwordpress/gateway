129ab447107c7d56f45b5c739cb6af0dc192d793
Casey Milne <casey@arcwp.ca>
2026-05-26 13:11:11 -0700

    Remove AI settings, fix settings buttons, rebuild.

--- react/apps/studio/build/index.asset.php
+++ react/apps/studio/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '0e0772c6eae96727f1c8');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '3fff7df3a27201d5983e');
