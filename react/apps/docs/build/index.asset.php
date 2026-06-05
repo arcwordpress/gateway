2d3a243b86714c7495ae1b40d468423e1ba21551
Casey Milne <casey@arcwp.ca>
2026-06-05 13:28:49 -0700

    Rebuild docs app.

--- react/apps/docs/build/index.asset.php
+++ react/apps/docs/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '7a3aa3591cf8a7b81222');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'ffe8091eb6eb61149f3f');
